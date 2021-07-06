1d58279538e68492eda7843eebdf083010240663
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-07-05 22:03:42 -0300

    Basic Integration testing

---
+++ Tweetbook.IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Tweetbook.Contracts;
+using Tweetbook.Contracts.v1.Requests;
+using Tweetbook.Contracts.v1.Responses;
+using Tweetbook.Data;
+
+namespace Tweetbook.IntegrationTests
+{
+    public class IntegrationTest
+    {
+        protected readonly HttpClient TestClient;
+
+        protected IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        services.RemoveAll(typeof(DataContext));
+                        services.AddDbContext<DataContext>(options => { options.UseInMemoryDatabase("TestDb"); });
+                    });
+                });
+            TestClient = appFactory.CreateClient();
+        }
+
+        protected async Task AuthenticateAsync()
+        {
+            TestClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await GetJwtAsync());
+        }
+
+        protected async Task<PostResponse> CreatePostAsync(CreatePostRequest request)
+        {
+            var response = await TestClient.PostAsJsonAsync(ApiRoutes.Posts.Create, request);
+            return await response.Content.ReadAsAsync<PostResponse>();
+        }
+
+        protected async Task<string> GetJwtAsync()
+        {
+            var response = await TestClient.PostAsJsonAsync(ApiRoutes.Identity.Register, new UserRegistrationRequest
+            {
+                Email = "test@integration.com",
+                Password = "@Test123!"
+            });
+
+            var registrationResponse = await response.Content.ReadAsAsync<AuthSuccessResponse>();
+            return registrationResponse.Token;
+        }
+    }
+}
---
+++ Tweetbook.IntegrationTests/PostsControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Tweetbook.Contracts;
+using Tweetbook.Contracts.v1.Requests;
+using Tweetbook.Domain;
+using Xunit;
+
+namespace Tweetbook.IntegrationTests
+{
+    public class PostsControllerTest : IntegrationTest
+    {
+        [Fact]
+        public async Task GetAll_WithoutAnyPosts_ReturnsEmptyReponse()
+        {
+            await AuthenticateAsync();
+
+            var response = await TestClient.GetAsync(ApiRoutes.Posts.GetAll);
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            (await response.Content.ReadAsAsync<List<Post>>()).Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task Get_ReturnsPost_WhenPostExistsInTheDatabase()
+        {
+            var postName = "Test Post";
+            await AuthenticateAsync();
+            var createdPost = await CreatePostAsync(new CreatePostRequest { Name = postName });
+
+            var response = await TestClient.GetAsync(ApiRoutes.Posts.Get.Replace("{id}", createdPost.Id.ToString()));
+
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var returnedPost = await response.Content.ReadAsAsync<Post>();
+            returnedPost.Id.Should().Be(createdPost.Id);
+            returnedPost.Name.Should().Be(postName);
+        }
+    }
+}
